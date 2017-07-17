257fdeb431759abd5568cf98e672d1504b6a1772
Rachel <rmcavin@gmail.com>
2017-07-17 16:10:12 -0500

    account deleted edge case solved and added

--- twitch.js
+++ twitch.js
@@ -1,4 +1,4 @@
-var users = ["brunofin","roxybear_gaming", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var users = ["brunofin","roxybear_gaming", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas","comster404"];
 var urlS = "https://wind-bow.gomix.me/twitch-api/streams/";
 var urlC = "https://wind-bow.gomix.me/twitch-api/channels/";
 var html = "";
@@ -9,33 +9,41 @@ var html = "";
 for (let i=0;i<users.length;i++) {
 
   $.getJSON(`${urlS + users[i]}?callback=?`, function(data) {
-            //html += `<div class = "userObject"> <div class = "userName"><h2> ${users[i]} </h2> </div>`;
-
+    $.getJSON(`${urlC + users[i]}?callback=?`, function(data2) {
+        console.log(data2);
             if (data.stream === null) {
-              html += `<div class = "userObject offline"> <div class = "userName"><h2> ${users[i]} </h2> </div><a href = 'http://twitch.tv/${users[i]}' target='_blank'><h4><i class="fa fa-circle noStream" aria-hidden="true"></i> Not currently streaming.</h4></a><img class = userLogo src = './twitch.png'></div>`
+              if (data2.error === "Not Found") {
+                html += `<div class = "userObject notFound"> <div class = "userName"><h2> ${users[i]} </h2> </div> <h4><i class="fa fa-circle noUser" aria-hidden="true"></i>Account Not Found</h4><img class = "userLogo" src = './twitch.png'></div>`
+              }
+              else {
+                html += `<div class = "userObject offline"> <div class = "userName"><h2> ${users[i]} </h2> </div> <a href = 'http://twitch.tv/${users[i]}' target='_blank'><h4><i class="fa fa-circle noStream" aria-hidden="true"></i> Not currently streaming.</h4></a><img class = userLogo src = './twitch.png'></div>`
+              }
             }
             else {
-              html += `<div class = "userObject online"> <div class = "userName"><h2> ${users[i]} </h2> </div><a href = 'http://twitch.tv/${users[i]}' target='_blank'><h4><i class="fa fa-circle yesStream" aria-hidden="true"></i>Streaming ${data.stream.game}</h4></a><img class = "userLogo" src = ${data.stream.channel.logo} alt = "channel logo"></div>`
+              html += `<div class = "userObject online"> <div class = "userName"><h2> ${users[i]} </h2> </div> <a href = 'http://twitch.tv/${users[i]}' target='_blank'><h4><i class="fa fa-circle yesStream" aria-hidden="true"></i>Streaming ${data.stream.game}</h4></a><img class = "userLogo" src = ${data.stream.channel.logo} alt = "channel logo"></div>`
 
             }
               $(".accountRow").html(html);
 
-
+        })
     })
   }
 
 
   $("#offlineButton").click(function() {
     $(".offline").show(1000);
+    $(".notFound").hide(1000);
     $(".online").hide(1000);
   })
 
   $("#onlineButton").click(function() {
     $(".online").show(1000);
+    $(".notFound").hide(1000);
     $(".offline").hide(1000);
   })
 
   $("#allButton").click(function() {
     $(".online").show(1000);
+    $(".notFound").show(1000);
     $(".offline").show(1000);
   })
